ba85ec46a2df8a79f3ab4c64c5dff8594a8e159f
tunafysh <hanansmani@gmail.com>
2024-05-17 23:40:16 +0200

    added capacitor to make it as a mobile app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tunafysh.tictactoe',
+  appName: 'tictactoe',
+  webDir: 'out'
+};
+
+export default config;
