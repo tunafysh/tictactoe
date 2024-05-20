19354e0ba266096377e58bdb7b7c5443662e46dc
tunafysh <hanansmani@gmail.com>
2024-05-20 10:28:29 -0700

    would rather make the api with elysia than nextjs. idc if i'm incompetent.

--- app/api/player/route.ts
+++ app/api/player/route.ts
@@ -1,27 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
-import * as firebase from "firebase/app"
+import { Elysia } from 'elysia'
 
-//TODO Add firebase integration
+const app = new Elysia({prefix: "/api/player"})
+app.get('/',({ cookie: { player } }: { cookie: { player: { value: string } } }) => {
+    return player.value
+})
 
-// Handle GET requests
-export async function GET(req: NextRequest) {
-  // Retrieve the value of the "player" cookie
-  const playerCookie = req.cookies.get('player');
-  return NextResponse.json({ player: playerCookie });
-}
+app.post("/", ({ cookie: { player } }: { cookie: { player: { value: string } } }) => {
+  player.value = ''  
+})
 
-// Handle POST requests
-export async function POST(request: NextRequest) {
-  const { player } = await request.json(); // Get the value from the request body
-
-  // Set the "player" cookie
-  const response = NextResponse.json({ message: 'Cookie '+ player+ ' set successfully' }, { status: 200 });
-  response.cookies.set({
-      name: 'player',
-      value: player,
-      httpOnly: true,
-      secure: false // Set to true in production
-  });
-
-  return response;
-}
+export const GET = app.handle 
+export const POST = app.handle 
