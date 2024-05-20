16b1a0e65a5ac572673e6cc759840f667ebc99b5
Trinitty999 <hanansmani@gmail.com>
2024-05-20 13:48:39 +0200

    Created a simple API to manage users and cookies. Will add firebase integration

---
+++ app/api/player/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+//TODO Add firebase integration
+
+// Handle GET requests
+export async function GET(req: NextRequest) {
+  // Retrieve the value of the "player" cookie
+  const playerCookie = req.cookies.get('player');
+  return NextResponse.json({ player: playerCookie });
+}
+
+// Handle POST requests
+export async function POST(request: NextRequest) {
+  const { player } = await request.json(); // Get the value from the request body
+
+  // Set the "player" cookie
+  const response = NextResponse.json({ message: 'Cookie '+ player+ ' set successfully' }, { status: 200 });
+  response.cookies.set({
+      name: 'player',
+      value: player,
+      httpOnly: true,
+      secure: false // Set to true in production
+  });
+
+  return response;
+}
