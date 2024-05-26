6713199ab65bb433787e88b737a095bdd4fb88d8
tunafysh <hanansmani@gmail.com>
2024-05-26 20:44:09 +0200

    testing.

--- components/authui.tsx
+++ components/authui.tsx
@@ -1,110 +1,123 @@
+"use client";
+import React from "react";
+import { Label } from "@/components/ui/auth-label";
+import { Input } from "@/components/ui/auth-input";
+import { cn } from "@/lib/utils";
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"  
-import { Dispatch, SetStateAction } from "react"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { BASE_API_URL } from "@/lib/constants"
-import {  useState } from "react"
-import { ExclamationTriangleIcon, GitHubLogoIcon } from "@radix-ui/react-icons"
-import { Alert, AlertDescription, AlertTitle } from "./ui/alert"
+  IconBrandGithub,
+  IconBrandGoogle,
+  IconBrandOnlyfans,
+} from "@tabler/icons-react";
 
-async function onSubmit(user: string, pass: string) {
-        // Get input value
-        const username = user
-        const password = pass
-        if(username !== null && password !== null) {
-
-          window.location.reload()
-
-          // Create a JSON object
-          
-          // Send data to the API
-          try {
-            const response = await fetch(BASE_API_URL+"/api/player", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              "body": `${username}`
-              });
-              
-              if (response.ok) {
-                
-              } else {
-                console.error("Error sending data:", response.status);
-              }
-            } catch (error) {
-              console.error("Error:", error);
-            };
-          }
-        }
-export function SignUp({ matchpass }: { matchpass: Dispatch<SetStateAction<boolean>> }) {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [confirm, setConfirm] = useState<string>("");
-
+export function SignupFormDemo() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className="absolute animate-fade top-4 left-4">Sign up</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle className="text-center">Sign up</DialogTitle>
-        </DialogHeader>
-        <Button><GitHubLogoIcon className="mr-2 h-4 w-4" />Sign up with github</Button>
-        <div className="flex flex-row items-center justify-between">
-        <hr className=" w-24"/>
-          <p className="text-center break-keep text-nowrap"> or the traditional way</p>
-        <hr className="w-24"/>
+    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+        Welcome to Aceternity
+      </h2>
+      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        Login to aceternity if you can because we don&apos;t have a login flow
+        yet
+      </p>
+
+      <form className="my-8" onSubmit={handleSubmit}>
+        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+          <LabelInputContainer>
+            <Label htmlFor="firstname">First name</Label>
+            <Input id="firstname" placeholder="Tyler" type="text" />
+          </LabelInputContainer>
+          <LabelInputContainer>
+            <Label htmlFor="lastname">Last name</Label>
+            <Input id="lastname" placeholder="Durden" type="text" />
+          </LabelInputContainer>
         </div>
-        <div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-              Username
-            </Label>
-            <Input onChange={(e) => setUsername(e.target.value)} className="col-span-3"/>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-              Password
-            </Label>
-            <Input onChange={(e) => setPassword(e.target.value)} className="col-span-3" type="password"/>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-             Confirm
-            </Label>
-            <Input onChange={(e) => setConfirm(e.target.value)} className="col-span-3" type="password"/>
-          </div>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" placeholder="••••••••" type="password" />
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="twitterpassword">Your twitter password</Label>
+          <Input
+            id="twitterpassword"
+            placeholder="••••••••"
+            type="twitterpassword"
+          />
+        </LabelInputContainer>
+
+        <button
+          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          type="submit"
+        >
+          Sign up &rarr;
+          <BottomGradient />
+        </button>
+
+        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+
+        <div className="flex flex-col space-y-4">
+          <button
+            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+          >
+            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              GitHub
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+          >
+            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Google
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+            type="submit"
+          >
+            <IconBrandOnlyfans className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              OnlyFans
+            </span>
+            <BottomGradient />
+          </button>
         </div>
-        <Button onClick={() => {
-          if( password === confirm) {
-          
-            onSubmit(username, password)
-            matchpass(false)
-          }
-          else {
-            matchpass(false)
-          }
-        }}>Sign up</Button>
-      </DialogContent>
-    </Dialog>
-    )
+      </form>
+    </div>
+  );
 }
 
-export function SignIn() {
+const BottomGradient = () => {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className="absolute animate-fade top-4 left-4">Sign in</Button>
-      </DialogTrigger>
-    </Dialog>
-  )
-}
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
