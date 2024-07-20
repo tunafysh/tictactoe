2cc1ae5f968a2925fe014b4ca822e65af4e337ae
Hanan <hanansmani@gmail.com>
2024-07-20 12:19:17 +0200

    BRUH

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
 
-const handler = NextResponse.json({ message: "alpha feature. will be available soon" }, { status: 200 })
-
-export { handler as GET, handler as POST }
+export default function POST() {
+    return NextResponse.json({ message: "alpha feature. will be available soon" }, { status: 200 })
+} 
