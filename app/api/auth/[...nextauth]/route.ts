453c2fb44bd915ffe11ebda372cb2a84decbc4b3
Hanan <hanansmani@gmail.com>
2024-07-20 12:16:10 +0200

    idk man

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
+import { NextResponse } from "next/server"
 
 
-const handler = "alpha feature. will be available soon"
+const handler = NextResponse.json({ message: "alpha feature. will be available soon" }, { status: 200 })
 
 export { handler as GET, handler as POST }
