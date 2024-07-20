cfb6ca58fbe2474c15be9f7df415a421ca958d6b
Hanan <hanansmani@gmail.com>
2024-07-20 12:21:47 +0200

    ugh

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server"
 
+export function GET() {
+    return NextResponse.json({ message: "alpha feature. will be available soon" }, { status: 200 })
+  }
 
 export default function POST() {
     return NextResponse.json({ message: "alpha feature. will be available soon" }, { status: 200 })
