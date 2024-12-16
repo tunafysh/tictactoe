bc0f386e2026a800e5950a0bfb965637d747350e
tunafysh <hanansmani@gmail.com>
2024-12-16 23:47:20 +0100

    mitigated one stupidity

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -116,7 +116,7 @@ return (
           <button
             className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
             onClick={() => {
-              login("github", formdata).then(() => router.push("/"))
+              login("github").then(() => router.push("/"))
             }}
             >
             <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
