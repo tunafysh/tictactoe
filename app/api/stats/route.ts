eaf9ca28782897aba5768ea0556cb552e4511b6d
tunafysh <hanansmani@gmail.com>
2024-12-17 13:56:24 +0300

    mistake again

--- app/api/stats/route.ts
+++ app/api/stats/route.ts
@@ -2,31 +2,21 @@ import { db, profiles } from "@/schema"
 import { NextRequest, NextResponse } from "next/server"
 import { eq, sql } from "drizzle-orm"
 
-export async function profileHandler(req: NextRequest) {
+export async function GET(req: NextRequest) {
     const searchparams = req.nextUrl.searchParams
     const id = searchparams.get("id")
     const action = searchparams.get("action")
-    if (id === "null" || action === null) {
-        return NextResponse.json({ error: "Malformed URL" }, { status: 500 })
-    }else{        
-        if(req.method === "GET") {
-            let result = await db.select(action == "games" ? { games: profiles.games } : action == "wins" ? { wins: profiles.wins }: {admin: profiles.admin}).from(profiles).where(eq(profiles.userId, id??"null"))
-            return NextResponse.json(result[0].games ?? result[0].wins ?? result[0].admin)
-        } 
-        else if(req.method === "POST") {
-            if( action === "admin"){
-                return NextResponse.json({ error: "You cannot become an admin." }, { status: 500 })
-            }
-            let result = await db.update(profiles).set(action == "games" ? { games: sql`${profiles.games} + 1` } : { wins: sql`${profiles.wins} + 1` }).where(eq(profiles.userId, id??"null"))
-            return NextResponse.json(result)
-        }
-    }
-}
-
-export async function GET(req: NextRequest) {
-    return profileHandler(req)
+    let result = await db.select(action == "games" ? { games: profiles.games } : action == "wins" ? { wins: profiles.wins }: {admin: profiles.admin}).from(profiles).where(eq(profiles.userId, id??"null"))
+    return NextResponse.json(result[0].games ?? result[0].wins ?? result[0].admin)
 }
 
 export async function POST(req: NextRequest) {
-    return profileHandler(req)
+    const searchparams = req.nextUrl.searchParams
+    const id = searchparams.get("id")
+    const action = searchparams.get("action")
+    if( action === "admin"){
+        return NextResponse.json({ error: "You cannot become an admin." }, { status: 500 })
+    }
+    let result = await db.update(profiles).set(action == "games" ? { games: sql`${profiles.games} + 1` } : { wins: sql`${profiles.wins} + 1` }).where(eq(profiles.userId, id??"null"))
+    return NextResponse.json(result)
 }
