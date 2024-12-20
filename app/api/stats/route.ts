bea3d128942afc4c3ce29c2233671f553eeebda4
tunafysh <hanansmani@gmail.com>
2024-12-20 20:22:28 +0100

    fixed the whole db issue

--- app/api/stats/route.ts
+++ app/api/stats/route.ts
@@ -1,3 +1,4 @@
+import { db, profiles, users } from "@/schema"
 import { NextRequest, NextResponse } from "next/server"
 import { eq, sql } from "drizzle-orm"
 
@@ -5,12 +6,19 @@ export async function GET(req: NextRequest) {
     const searchparams = req.nextUrl.searchParams
     const id = searchparams.get("id")
     const action = searchparams.get("action")
+    console.log(id)
+    console.log(action)
     if( id === null || action === null){
         return NextResponse.json({ error: "Invalid request." }, { status: 500 })
     }else {
         let uid = await crosscheckIdToUserId(id)
-        let result = await sql`SELECT "${action}" FROM "profile" WHERE userid="${uid}";`
-        return NextResponse.json(result)
+        let query = await db.select(action == "games" ? { games: profiles.games } : action == "wins" ? { wins: profiles.wins }: {admin: profiles.admin}).from(profiles).where(eq(profiles.userid, uid))
+        console.log(query)
+        if(query.length === 0){
+            await db.insert(profiles).values({userid: uid, games: 0, wins: 0, admin: false})
+        }
+        let result = action == "games"? query[0].games: action == "wins"? query[0].wins: query[0].admin
+        return NextResponse.json(query.length != 0? result: "User created")
     }
 }
 
@@ -18,6 +26,8 @@ export async function POST(req: NextRequest) {
     const searchparams = req.nextUrl.searchParams
     const id = searchparams.get("id")
     const action = searchparams.get("action")
+    console.log(id)
+    console.log(action)
     if( action === "admin"){
         return NextResponse.json({ error: "You cannot become an admin." }, { status: 500 })
     }
@@ -26,13 +36,14 @@ export async function POST(req: NextRequest) {
     }
     else{
         let uid = await crosscheckIdToUserId(id)
-        let currentValue = await sql`SELECT "${action}" FROM "profile" WHERE userid="${uid}";`
-        let result = await sql`UPDATE "profile" SET games=${currentValue} + 1 WHERE userid="${uid}";`
-        return NextResponse.json(result)
+        let query = await db.update(profiles).set(action == "games" ? { games: sql`${profiles.games} + 1` } : { wins: sql`${profiles.wins} + 1` }).where(eq(profiles.userid, uid))
+        return NextResponse.json(query)
     }
 }
 
 async function crosscheckIdToUserId(id: string){
-    let uid = await sql`SELECT userid FROM "user" WHERE id="${id}";`
+    let result = await db.select({ userid: users.userid }).from(users).where(eq(users.id, id))
+    let uid = result[0].userid
+    console.log(uid)
     return uid
 }
